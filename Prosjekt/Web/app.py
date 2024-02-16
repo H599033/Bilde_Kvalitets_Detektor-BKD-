259a333c36f4fe54cebef04682829b9c0b7e4e4d
586640 <bjornell10@gmail.com>
2024-02-16 13:35:22 +0100

    Basic fungerende nettside som lister ut biler.

--- Prosjekt/Web/app.py
+++ Prosjekt/Web/app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, Response
-import time
+from flask import Flask, render_template, jsonify
+import os
+import pickle
+import DbService 
+import os
+
+
 
 app = Flask(__name__)
 
@@ -7,18 +12,24 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/stream')
-def stream():
-    def event_stream():
-        while True:
-            # Call your function that detects and saves cars
-            # This is just an example, you'd want to replace this with the actual call
-            msg = detect_and_save(image_path, output_path)
-            yield msg # EKSEMPEL PÅ BRUK: yield 'A new car has been detected!\n\n'
-                    # sett yield i funksjonen som skal sende ett bilobjekt her som skal vises på nettsiden. EKS: lagbil() { yield car}
-            time.sleep(1)  # you can adjust this as needed
-
-    return Response(event_stream(), mimetype='text/event-stream')
+@app.route('/display')
+def display():
+    # Get all the Bil objects
+    biler = DbService.hent_alle_biler()
+    
+    # Prepare the data for JSON
+    data = []
+    for bil in biler:
+        data.append({
+            'ID': bil.ID,
+            'tid': bil.tid,
+            'dato': bil.dato,
+            'sted': bil.sted,
+            'orginal_bilder': bil.orginal_bilder,
+            'redigerte_bilder': bil.redigerte_bilder
+        })
+    
+    return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    app.run(debug=True)
